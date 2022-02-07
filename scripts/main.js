e1c5224cbc2d64de57a1041da0e0c8ff6865ecde
Caleb Pardeik <7912201@isd535.org>
2022-02-07 11:28:54 -0600

    AD phaser AD sprites AD

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,52 @@
-// Put your JavaScript here
+let config = {
+    type: Phaser.AUTO,
+    witdh: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let game = new Phaser.Game(config);
+let platforms;
+let player;
+
+function preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", 
+        { frameWidth: 32, frameHeight: 48 });
+}
+
+function create() {
+    this.add.image(400, 300, "sky")
+    
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    platforms.create(600, 400, "ground");
+    platforms.create(50, 250, "ground");
+    platforms.create(750, 250, "ground");    
+
+    player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true)
+}
+
+function update() {
+
+}
+
