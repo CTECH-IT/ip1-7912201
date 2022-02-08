9ca22c21fcfb74f3e21a46f570870edcfb173e2c
Caleb Pardeik <7912201@isd535.org>
2022-02-08 11:12:51 -0600

    AD movement AD stars CY width to be better

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 let config = {
     type: Phaser.AUTO,
-    witdh: 800,
+    width: 800,
     height: 600,
     physics: {
         default: "arcade",
@@ -19,6 +19,8 @@ let config = {
 let game = new Phaser.Game(config);
 let platforms;
 let player;
+let cursors;
+let stars;
 
 function preload() {
     this.load.image("sky", "assets/sky.png");
@@ -44,38 +46,81 @@ function create() {
 
     player.setBounce(0.2);
     player.setCollideWorldBounds(true)
-}
 
-this.anims.create({
-    key: "left",
+    this.anims.create({
+        key: "left",
+    
+        frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3}),
+    
+        frameRate: 10,
+    
+        repeat: -1
+    });
+    
+    this.anims.create({
+        key: "turn",
+    
+        frames: [{key: "dude", frame: 4}],
+    
+        frameRate: 20,
+    });
+    
+    this.anims.create({
+        key: "right",
+    
+        frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8}),
+    
+        frameRate: 10,
+    
+        repeat: -1
+    });
 
-    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3}),
+    this.physics.add.collider(player, platforms);
 
-    frameRate: 10,
+    cursors = this.input.keyboard.createCursorKeys();
 
-    repeat: -1
-});
+    stars = this.physics.add.group({
+        key: "star",
+        repeat: 11,
+        setXY: {x: 12, y: 0, stepX: 70}
+    })
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
 
-this.anims.create({
-    key: "turn",
+    this.physics.add.collider(stars, platforms);
 
-    frames: this.anims.generateFrameNumbers({key: "dude", frame: 4}),
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 
-    frameRate: 20,
-});
 
-this.anims.create({
-    key: "right",
 
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8}),
+};
 
-    frameRate: 10,
 
-    repeat: -1
-});
 
 
 function update() {
 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play("right", true);
+    } else {
+        player.setVelocityX(0);
+
+        player.anims.play("turn");
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+
 };
 
+function collectStar(player, star) {
+    star.disableBody(true, true)
+}
