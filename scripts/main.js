69b9916fe09ce625c302f50f2bbdcccb1a4c4aa6
Caleb Pardeik <7912201@isd535.org>
2022-02-08 11:32:23 -0600

    AD enemies AD respawn stars

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,7 @@ let cursors;
 let stars;
 let score = 0;
 let scoreText = "";
+let bombs;
 
 function preload() {
     this.load.image("sky", "assets/sky.png");
@@ -96,6 +97,9 @@ function create() {
 
     scoreText = this.add.text(16, 16, "score: 0", {fontSize: "32px", fill: "#000" });
 
+    bombs = this.physics.add.group();
+    this.physics.add.collider(bombs, platforms);    
+    this.physics.add.collider(player, bombs, hitBomb, null, this);    
 };
 
 
@@ -128,4 +132,25 @@ function collectStar(player, star) {
 
     score += 10;
     scoreText.setText("Score: " + score);
+
+    if(stars.countActive(true) === 0) {
+        stars.children.iterate(function (child) {
+            child.enableBody(true, child.x, 0, true, true);
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, "bomb");
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+}
+
+function hitBomb(player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play("turn");
+
+    gameOver = true;
 }
